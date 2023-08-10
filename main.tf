05b8b94e4289d2f8846d45d99b27dffaccdb6482
Eugene Marchanka <eugene.marchanka@berkshiregrey.com>
2023-08-10 13:34:00 -0400

    try guestinfo

--- main.tf
+++ main.tf
@@ -88,8 +88,8 @@ resource "vsphere_virtual_machine" "this" {
 
   vapp {
     properties = {
-      # "talos.config" = module.userdata[count.index].configuration
-      "talos.config" = local.config_file
+      "guestinfo.talos.config" = module.userdata[count.index].configuration
+      # "guestinfo.talos.config" = local.config_file
     }
   }
 
